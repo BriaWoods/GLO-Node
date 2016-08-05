2383dd8809925e4567ef119248cdb15cf94d024b
Stephen Looney <stephen@shlooney.com>
2016-08-04 19:15:18 -0500

    adding a function to cloud code, and going to try calling it from iOS app

---
+++ cloud/main.js
@@ -0,0 +1,20 @@
+
+
+Parse.Cloud.define("logIn", function(req, res) {
+/*	Parse.Cloud.useMasterKey();
+
+		var phoneNumber = req.params.phoneNumber;
+			phoneNumber = phoneNumber.replace(/\D/g, '');
+
+			if (phoneNumber && req.params.codeEntry) {
+				Parse.User.logIn(phoneNumber, secretPasswordToken + req.params.codeEntry).then(function (user) {
+					res.success(user.getSessionToken());
+				}, function (err) {
+					res.error(err);
+				});
+			} else {
+				res.error('Invalid parameters.');
+			}
+			*/
+	console.log('Here I am in the Parse Cloud Code Login Function');
+});
