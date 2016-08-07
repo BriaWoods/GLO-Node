5a3913f561cc9fb282ec1e64c44b14b694f12b34
Stephen Looney <stephen@shlooney.com>
2016-08-07 18:53:23 -0500

    Parse variable should be global, going to try not initializing it

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,10 @@ var mongoose = require('mongoose');
 var request = require('request');
 var querystring = require('querystring');
 
-var Parse = require('parse/node');
+//var Parse = require('parse/node');
 
 
-Parse.initialize("3DSGLOBALROUNDUP");
+//Parse.initialize("3DSGLOBALROUNDUP");
 Parse.serverURL = 'https://glo-app.heroku.com/parse'
 
 /* Twilio stuff */
